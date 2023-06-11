9c5f91fd385d5cf2884b289a274d2a5ceefaa131
sonam-niit <sonamlovesmumypapa@gmail.com>
2023-06-11 23:07:03 +0530

    first commit

---
+++ Session-2/array.js
@@ -0,0 +1,4 @@
+const array=[1,2,3,4,5,6,7,8,9];
+
+console.log(array.map((value)=>value*2));
+console.log(array.filter((value)=>value%3==2).length);
---
+++ Session-2/first.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+
+function add(a,b){
+    return a+b;
+}
+const result= add(12,34);
+console.log(`Result is ${result}`)
+//directl call function from console
+console.log(`Result is ${add(67,89)}`);
